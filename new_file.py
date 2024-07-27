ce956ec741740f6c1eca767dc3b12a344cc08a5a
sausissimo <pinchermaria@gmail.com>
2024-07-27 19:27:13 +0600

    Added new_file.py with an example of decorator function

---
+++ new_file.py
@@ -0,0 +1,14 @@
+﻿# Пример декоратора: 
+# функции, которая принимает другие функции и меняет их функционал
+
+def shout(text):
+    return text.upper()
+
+def whisper(text):
+    return text.lower()
+
+def greet(func):
+    print(func("Кричу или шепчу?"))
+
+greet(whisper)
+greet(shout)
